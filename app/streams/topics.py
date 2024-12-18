a7825fd27dd10b9e074ea6571c407a2d54e8037c
Vitaly Mikhailov <vmikhailov@reksoft.ru>
2024-12-18 22:47:51 +0300

    schemas

--- app/streams/topics.py
+++ app/streams/topics.py
@@ -1,7 +1,8 @@
 from .config import Config
 from .handler import app
+from .schemas import message_schema
 
-messages_in = app.topic(Config.topic_messages_in)
-messages_out = app.topic(Config.topic_messages_out)
+messages_in = app.topic(Config.topic_messages_in, schema=message_schema)
+messages_out = app.topic(Config.topic_messages_out, schema=message_schema)
 
 bans = app.topic(Config.bans_topic)
